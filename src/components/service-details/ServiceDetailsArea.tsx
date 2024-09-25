82fb61a03b391368aeb7b05b0c5205c5239b8545
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2024-09-25 18:23:32 -0300

    user cliente

--- src/components/service-details/ServiceDetailsArea.tsx
+++ src/components/service-details/ServiceDetailsArea.tsx
@@ -1,23 +1,41 @@
-import VideoPopup from '@/modals/VideoPopup';
-import React, { useState } from 'react';
+'use client'
+
+import VideoPopup from '@/modals/VideoPopup'
+import React, { useState } from 'react'
 
 const ServiceDetailsArea = ({ service }) => {
-  const [isVideoOpen, setIsVideoOpen] = useState(false);
+  const [isVideoOpen, setIsVideoOpen] = useState(false)
 
   return (
     <>
       <section className="service_area section-padding">
         <div className="container">
           <div className="row">
-            <div className="col-lg-10 offset-lg-1 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
+            <div
+              className="col-lg-10 offset-lg-1 col-sm-12 col-xs-12 wow fadeInUp"
+              data-wow-duration="1s"
+              data-wow-delay="0.2s"
+              data-wow-offset="0"
+            >
               <div className="single_service_details">
                 <img src={service.image} className="img-fluid" alt="image" />
                 <h2>{service.title}</h2>
                 <p>{service.description}</p>
               </div>
               {/* Resto do conteúdo */}
-              <div className="video-area" style={{ backgroundImage: `url(assets/img/video.jpg)`, backgroundSize: "cover", backgroundPosition: "center center" }}>
-                <a onClick={() => setIsVideoOpen(true)} style={{ cursor: "pointer" }} className="magnific_popup video-button">
+              <div
+                className="video-area"
+                style={{
+                  backgroundImage: `url(assets/img/video.jpg)`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center center'
+                }}
+              >
+                <a
+                  onClick={() => setIsVideoOpen(true)}
+                  style={{ cursor: 'pointer' }}
+                  className="magnific_popup video-button"
+                >
                   <i className="ti-image"></i>
                 </a>
               </div>
@@ -25,9 +43,13 @@ const ServiceDetailsArea = ({ service }) => {
           </div>
         </div>
       </section>
-      <VideoPopup isVideoOpen={isVideoOpen} setIsVideoOpen={setIsVideoOpen} videoId={'i810CxN5Q6Q'} />
+      <VideoPopup
+        isVideoOpen={isVideoOpen}
+        setIsVideoOpen={setIsVideoOpen}
+        videoId={'i810CxN5Q6Q'}
+      />
     </>
-  );
-};
+  )
+}
 
-export default ServiceDetailsArea;
+export default ServiceDetailsArea
